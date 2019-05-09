84a5e199aa0a5c8ebf35ecf9a20dbc5b54f012db
Tiago Marzari <tiago.marzari@tools.com.br>
2019-05-09 16:16:03 -0300

    Criação arquivo pom e arquivo de teste

---
+++ src/test/java/br/com/automation/APIRest/TestAPI.java
@@ -0,0 +1,43 @@
+package br.com.automation.APIRest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Test;
+
+import io.restassured.http.ContentType;
+
+public class TestAPI {
+
+	@Test
+	public void TesteGET() {
+		String uriBase = "https://postman-echo.com/get";
+		given()
+			.relaxedHTTPSValidation()
+			.param("foo1", "bar1")
+			.param("foo2", "bar2")
+		.when()
+			.get(uriBase)
+		.then()
+			.statusCode(200) // O status http retornado foi 200
+			.contentType(ContentType.JSON) // O response foi retornado no formato JSON
+			.body("headers.host", equalTo("postman-echo.com")) // A chave “host” possui exatamente o valor postman-echo.com”
+			.body("args.foo1", containsString("bar")); // A chave “foo1” contém o valor “bar”
+	}
+	
+	@Test
+	public void TestePOST() {
+		String uriBase = "https://postman-echo.com/post";
+		given()
+			.relaxedHTTPSValidation().body("{ foo1: bar3, foo2: bar2 }")
+		.when()
+			.post(uriBase)
+		.then()
+			.statusCode(200) // O status http retornado foi 200
+			.contentType(ContentType.JSON) // O response foi retornado no formato JSON
+			.body("headers.host", equalTo("postman-echo.com")) // A chave “host” possui exatamente o valor postman-echo.com”
+			.body("data", containsString("{ foo1: bar3, foo2: bar2 }")); // A chave “data” contém o valor "{ foo1: bar3, foo2: bar2 }"
+	}
+
+}
